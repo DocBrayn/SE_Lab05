3ed19b1751c078ec5fe2308c10fbd63a08e6e95f
Dmitry Barabanov <docbrayn008@mail.ru>
2021-04-13 17:42:07 +0500

    Added sum

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main()
 {
-    int a, b;
+    int a, b, c;
     cout << "Enter two integer numbers" << endl;
     cin >> a >> b;
+    c = a + b;
+    cout << "a + b = " << c << endl;
 }
